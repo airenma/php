b192804a9afcf4812c9a5ebcc31db57e41fcde4d
airenma <airenma@gmail.com>
2016-12-12 16:02:35 +0200

    refactoring

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+return[
+	'database' => [
+		'name' =>'mytodo',
+		'username' => 'root',
+		'pass' => 123,
+		'connection' => 'mysql:host=127.0.0.1',
+		'options' => [
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+		]
+
+	]
+];
