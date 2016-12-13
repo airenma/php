808e0fa47893578cd1ea86150b9cfab9434b9ff9
airenma <airenma@gmail.com>
2016-12-13 23:14:45 +0200

    new

--- bootsrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$config = require 'config.php';
-require 'database/connection.php';
-require 'database/QueryBuilder.php';
-
-return new QueryBuilder(
-	Connection::make($config['database'])
-	);
