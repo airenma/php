29f64bd47927800d2b15e0d5391b7b1ca91f3b9e
airenma <airenma@gmail.com>
2016-12-15 14:31:18 +0200

    blank

--- controllers/about-culture.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$name = 'Andrej Gladkov';
-
-require 'views/about-culture.view.php';
--- views/partials/nav.php
+++
@@ -1,8 +0,0 @@
-<nav>
-	<ol>
-		<li><a href="/">Home</a></li>
-		<li><a href="/about">About</a></li>
-		<li><a href="/about/culture">About our Culture</a></li>
-		<li><a href="/contact">Contact</a></li>
-	</ol>
-</nav>
